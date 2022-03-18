eecc1feac01f18e0553987b846380e910416d32a
Gabriel Duschinsky <gabdusch@gmail.com>
2022-03-17 23:11:03 -0400

    chore: add TS definitions for built-in icons

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,33 @@ export const Colors = {
 	ORANGE: rgb(255, 80, 0),
 }
 
-const Icons = require('../../../../lib/resources/icons')
+/**
+ * Temporary hack to get proper TypeScript support for built-in Base64 icons (and so that
+ * we don't have to import `InstanceSkel` everywhere) */
+const Icons = require('../../../../lib/resources/icons') as {
+	ICON_POWER_UNKNOWN: string
+	ICON_POWER_ON: string
+	ICON_POWER_OFF: string
+	ICON_FWD_ACTIVE: string
+	ICON_FWD_INACTIVE: string
+	ICON_REW_ACTIVE: string
+	ICON_REW_INACTIVE: string
+	ICON_REC_ACTIVE: string
+	ICON_REC_INACTIVE: string
+	ICON_STOP_INACTIVE: string
+	ICON_STOP_ACTIVE: string
+	ICON_PAUSE_INACTIVE: string
+	ICON_PAUSE_ACTIVE: string
+	ICON_PLAY_INACTIVE: string
+	ICON_PLAY_ACTIVE: string
+	ICON_UP: string
+	ICON_DOWN: string
+	ICON_LEFT: string
+	ICON_RIGHT: string
+	ICON_UP_RIGHT: string
+	ICON_DOWN_RIGHT: string
+	ICON_UP_LEFT: string
+	ICON_DOWN_LEFT: string
+}
 
 export { Icons }
